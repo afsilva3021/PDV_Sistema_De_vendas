1c452a43d65b5f27af703369a7c3f16a9fedfa5b
afsilva3021 <afsilva3021@gmail.com>
2025-06-05 08:48:03 -0300

    atualizações no projeto

--- app/Controllers/OrcamentoController.php
+++ app/Controllers/OrcamentoController.php
@@ -15,12 +15,41 @@ class OrcamentoController
 
   public function orcamento()
   {
+    $produtos = [
+      ['ID' => 1, 'NOME' => 'Produto 1', 'CODIGO' => '001', 'EAN' => '123456', 'DESCRICAO' => 'Descrição 1', 'PRECO' => 10.00, 'QUANTIDADE_ESTOQUE' => 5, 'STATUS' => 'ativo'],
+      // ... outros produtos ...
+    ];
+    $orcamento = isset($_SESSION['orcamento']) ? $_SESSION['orcamento'] : [];
+
+
     // Render the index view
     echo $this->twig->render('orcamento.html', [
       'user_name' => $_SESSION['user_name'],
-      'title' => 'Orçamento',
+      'title'  => 'Orçamento',
       'numped' => rand(0, 999999),
       'dthoje' => date('Y-m-d'),
+      'produtos' => $produtos,
+      'orcamento' => $orcamento
+    ]);
+
+    // 3. Gera o PDF com Dompdf
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
+
+    // 4. Exibe no navegador
+    $dompdf->stream("relatorio.pdf", ["Attachment" => false]); // false = abre no navegador
+  }
+
+  public function novoRcamento(): void
+  {
+    $produtos = $this->produtoModel->getAll(); // Busca todos os produtos
+    $listaOrcamento = isset($_SESSION['orcamento']) ? $_SESSION['orcamento'] : [];
+
+    echo $this->view->render('orcamento.html', [
+      'produtos' => $produtos,
+      'listaOrcamento' => $listaOrcamento
     ]);
   }
 }
